32c347dd1c0d838dd644dafa73529f5383c704ba
Ryan <55410510+RyanWestwood@users.noreply.github.com>
2022-09-25 13:07:05 +0100

    Added SDL libraries; SDL2 SDL_image SDL_mixer SDL_ttf

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <SDL_ttf.h>
 
-int main(){
-    std::cout << "Hello, world!\n";
-    return 0;
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char* args[])
+{
+	SDL_Window* window = NULL;
+	SDL_Surface* screenSurface = NULL;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		std::cout << "SDL could not initialize! SDL_Error: %s\n," << SDL_GetError();
+	}
+	else
+	{
+		window = SDL_CreateWindow("Artificial-Intelligence", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (window == NULL)
+		{
+			std::cout << "Window could not be created! SDL_Error: %s\n" << SDL_GetError();
+		}
+		else
+		{
+			screenSurface = SDL_GetWindowSurface(window);
+			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0x66, 0x66, 0x66));
+			SDL_UpdateWindowSurface(window);
+			SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
+		}
+	}
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
 }
