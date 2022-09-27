950a726682d060dbb6de2dc4f089ae997f5112ba
Ryan <55410510+RyanWestwood@users.noreply.github.com>
2022-09-27 20:28:38 +0100

    added sound support, improved logging for asset loading, renamed initialization functions for clarity

---
+++ src/engine/Sound.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <iostream>
+#include <SDL.h>
+#include <SDL_mixer.h>
+
+namespace Sound {
+	class SoundError : public std::exception {
+	public:
+		const char* what() const throw(){ return "Sound Loading Error!\n"; }		
+	};
+
+	bool InitalizeSound();
+	void UninitalizeSound();
+
+	Mix_Chunk* LoadSfx(const char* filename);
+	Mix_Music* LoadMusic(const char* filename);
+}
+
+class SoundEffect {
+public:
+	SoundEffect();
+
+	void Initialize(const char* filename);
+	void PlaySound();
+
+public:
+	Mix_Chunk* m_Sound;
+};
+
+class Music {
+public:
+	Music();
+
+	void Initialize(const char* filename);
+	void PlayMusic();
+
+public:
+	Mix_Music* m_Music;
+};
