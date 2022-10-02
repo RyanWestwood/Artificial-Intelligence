4a8ed36ebc2001feabe79014897f3812edad0840
Ryan <55410510+RyanWestwood@users.noreply.github.com>
2022-10-02 18:22:04 +0100

    animated foreground

--- src/Cooldown.cpp
+++ src/Cooldown.cpp
@@ -13,19 +13,19 @@ void Cooldown::Initialize(SDL_Point position)
 	m_Text.m_Dimensions.y = position.y - 4 + (64 - m_Text.m_Dimensions.h / 2) / 2;
 	
 	m_Distance = 100.f;
-	m_Timer = 0.f;
+	m_Timer = 2.f;
 	m_Cooldown = 2.f;
 }
 
 void Cooldown::Update(float delta_time)
 {
-	m_Timer += delta_time;
-
-	if (m_Timer >= m_Cooldown) {
+	m_Timer -= delta_time;
+	m_Foreground.m_Destination.h = 48 * ((m_Timer / m_Cooldown));
+	if (m_Timer <= 0.f) {
 #ifdef LOGGING
 		std::cout << "Cooldown!\n";
 #endif
-		m_Timer = 0.f;
+		m_Timer = m_Cooldown;
 	}
 }
 
