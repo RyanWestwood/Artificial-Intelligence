cf702bf6b26df7ddeb3e4bfa3835fbecc6e3e76c
Ryan <55410510+RyanWestwood@users.noreply.github.com>
2022-10-05 20:19:59 +0100

    added todo

--- src/Cooldown.cpp
+++ src/Cooldown.cpp
@@ -11,6 +11,8 @@
 #define TXO 6	// Text X Position Offset
 #define TYO 4	// Text Y Position Offset
 
+//	TODO; @RyanWestwood - Optimize this a little more. 
+//	Currently loading the same font n times when only the texture is needed!
 void Cooldown::Start()
 {
 	m_IsCountingDown = true;
