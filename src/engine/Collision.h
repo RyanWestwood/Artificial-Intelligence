1a683b8a9b9373a6aa2d4a8c4a16a972e1828fec
Ryan <55410510+RyanWestwood@users.noreply.github.com>
2022-10-06 11:41:38 +0100

    Collision

---
+++ src/engine/Collision.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "SDL.h"
+
+namespace COLLISION {
+
+	struct Circle {
+		SDL_Point m_Position;
+		int m_Radius;
+	};
+
+	bool BoxCollision(SDL_Rect& lhs, SDL_Rect& rhs);
+	bool SpeheCollision(Circle& lhs, Circle& rhs);
+	bool ComplexCollision(Circle& lhs, SDL_Rect& rhs);
+}
