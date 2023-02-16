0b0fe1ec0b6f39fbabf28950a14df5a04ea0f008
Ryan <55410510+RyanWestwood@users.noreply.github.com>
2023-02-16 03:40:20 +0000

    made build scripts easier

--- build-scripts/build-project.bat
+++ build-scripts/build-project.bat
@@ -1,6 +1,7 @@
 cd ../
 set working_dir=%cd%
 set library_binaries=%working_dir%/library_binaries
+set cmake="cmake"
 set generator="-G Visual Studio 17 2022"
 
 CALL :InstallLibs ai-library Release %library_binaries%
@@ -14,9 +15,9 @@ EXIT /B %ERRORLEVEL%
 :InstallProject
 if not exist "build" mkdir build
 cd build
-cmake %generator% -A x64 -DCMAKE_BUILD_TYPE=%~1 -S ./../ -DCMAKE_PREFIX_PATH=%~2 -D MY_VARIABLE=%~1
-cmake --build . --config %~1 --parallel
-cmake --install %working_dir%/build --prefix %~2 --config %~1
+%cmake% %generator% -A x64 -DCMAKE_BUILD_TYPE=%~1 -S ./../ -DCMAKE_PREFIX_PATH=%~2 -D MY_VARIABLE=%~1
+%cmake% --build . --config %~1 --parallel
+%cmake% --install %working_dir%/build --prefix %~2 --config %~1
 cd ../
 echo Project built successfully
 EXIT /B 0
@@ -26,9 +27,9 @@ echo Installing %~1
 cd external\%~1
 if not exist "build" mkdir build
 cd build
-cmake %generator% -A x64 -DCMAKE_BUILD_TYPE=%~2 -S %working_dir%/external/%~1 -B %working_dir%/external/%~1/build -DCMAKE_PREFIX_PATH=%~3
-cmake --build . --config %~2 --parallel
-cmake --install . --prefix %~3 --config %~2 
+%cmake% %generator% -A x64 -DCMAKE_BUILD_TYPE=%~2 -S %working_dir%/external/%~1 -B %working_dir%/external/%~1/build -DCMAKE_PREFIX_PATH=%~3
+%cmake% --build . --config %~2 --parallel
+%cmake% --install . --prefix %~3 --config %~2 
 cd ../../..
 echo %~1 Installed
 EXIT /B 0
--- build-scripts/run-tests.bat
+++ build-scripts/run-tests.bat
@@ -1,12 +1,14 @@
 cd ../
 set working_dir=%cd%
 set library_binaries=%working_dir%/library_binaries
+set cmake="cmake"
+set ctest="ctest"
 set generator="-G Visual Studio 17 2022"
 set configuration="Release"
 
 cd test
-cmake %generator% -A x64 -DCMAKE_BUILD_TYPE=%configuration% -S ./ -B "build" -DCMAKE_PREFIX_PATH=%library_binaries%
-cmake --build build --config %configuration% --parallel
+%cmake% %generator% -A x64 -DCMAKE_BUILD_TYPE=%configuration% -S ./ -B "build" -DCMAKE_PREFIX_PATH=%library_binaries%
+%cmake% --build build --config %configuration% --parallel
 cd build
-ctest -C %configuration%
+%ctest% -C %configuration%
 pause
