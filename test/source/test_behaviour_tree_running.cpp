ec770fdead5eb7766ca0a186c56ecde195e43fa6
Ryan <55410510+RyanWestwood@users.noreply.github.com>
2023-02-16 01:53:25 +0000

    Player bt test

--- test/source/test_behaviour_tree_running.cpp
+++ test/source/test_behaviour_tree_running.cpp
@@ -9,73 +9,93 @@
 
 #include <iostream>
 #include <random>
+#include <functional>
+#include <bitset>
 
-auto GetRandomProbability() {
-	std::random_device rd;
-	std::mt19937 gen(rd()); 
-	std::uniform_int_distribution<> distr(0, 99); 
-	return distr(gen);
-}
-
-class WalkToDoor : public Node {
+class Player {
 public:
-	WalkToDoor() = default;
-	Status Update() override {
-		if (m_Status == Status::Success) return Status::Success;
-		std::cout << "Walking to door!\n";
-		m_Status = Status::Success;
-		return Status::Success;
-	};
-};
+	Player(){
+		door_attempts = 0;
+	}
 
-class OpenDoor : public Node {
-public:
-	OpenDoor() = default;
-	Status Update() override {
-		if (action_time >= 10) {
+	void CreateBt() {
+
+		auto while_not_dead = [&]() -> Status {
+			//	bitset index = 4
+			if (tree_conditions[4]) return Status::Success; // Have this queried somehow
+			tree_conditions.reset();
+			door_attempts = 0;
+			return Status::Running;
+		};
+
+		auto open_door_lam = [&]() -> Status {
+			//	bitset index = 0
+			if (tree_conditions[0]) return Status::Success; // Have this queried somehow
+			if (door_attempts >= 10) {
 				std::cout << "Opening door!\n";
 				return Status::Success;
-		}
-		action_time++;
-		std::cout << "Attempting to open door!\n";
-		return Status::Running;
-	};
-private:
-	int action_time = 0;
-};
+			}
+			door_attempts++;
+			std::cout << "Attempting to open door!\n";
+			return Status::Running;
+		};
 
-class CloseDoor : public Node {
-public:
-	CloseDoor() = default;
-	Status Update() override {
-		std::cout << "Closing door!\n	\n";
-		return Status::Success;
-	};
+		auto walk_door_lam = [&]() -> Status {
+			//	bitset index = 1
+			if (tree_conditions[1]) return Status::Success; // Have this queried somehow
+			std::cout << "Walking to door!\n";
+			tree_conditions.set(1);
+			return Status::Success;
+		};
+
+		auto close_door_lam = [&]() -> Status {
+			//	bitset index = 2
+			if (tree_conditions[2]) return Status::Success; // Have this queried somehow
+			std::cout << "Closing door!\n	\n";
+			tree_conditions.set(2);
+			return Status::Success;
+		};
+
+		auto blackboard = ai::CreateBlackboard(globals::GetAssetDirectory() + "blackboards/door_test.csv");
+		auto door_sequence = NodeFactory::createCompositeNode<Sequence>(blackboard);
+
+		auto alive_condition = NodeFactory::createNode<Node>(blackboard, while_not_dead);
+		auto open_door = NodeFactory::createNode<Node>(blackboard, open_door_lam);
+		auto walk_to_door = NodeFactory::createNode<Node>(blackboard, walk_door_lam);
+		auto closing_door = NodeFactory::createNode<Node>(blackboard, close_door_lam);
+
+		door_sequence->AddNode(std::move(walk_to_door));
+		door_sequence->AddNode(std::move(open_door));
+		door_sequence->AddNode(std::move(closing_door));
+		door_sequence->AddNode(std::move(alive_condition));
+
+		tree = BehaviourTree(std::move(door_sequence));
+	}
+
+	void UpdateTree(){
+		tree.Update();
+	}
+
+private:
+	BehaviourTree tree;
+	std::bitset<8> tree_conditions;
+
+	int door_attempts = 0;
 };
 
 int main() {
 
 	bool globals = globals::InitializeGlobals();
 	if (!globals) return 1;
-	auto blackboard = ai::CreateBlackboard(globals::GetAssetDirectory() + "blackboards/door_test.csv");
 
-	auto door_sequence = NodeFactory::createCompositeNode<Sequence>(blackboard);
-
-	auto walk_to_door = NodeFactory::createNode<WalkToDoor>();
-	auto open_door = NodeFactory::createNode<OpenDoor>();
-	auto closing_door = NodeFactory::createNode<CloseDoor>();
-
-	door_sequence->AddNode(std::move(walk_to_door));
-	door_sequence->AddNode(std::move(open_door));
-	door_sequence->AddNode(std::move(closing_door));
-
-	BehaviourTree tree(std::move(door_sequence));
-
-	Status result = Status::Error;
-	do
+	Player carlo;
+	carlo.CreateBt();
+	
+	// Fake game loop
+	for (short i = 0; i <= 30; i++)
 	{
-		result = tree.Update();
-	} while (result != Status::Success);
+		carlo.UpdateTree();
+	}
 
 	return 0;
 }
