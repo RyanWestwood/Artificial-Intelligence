0e78ce41a332ebff86ce69ed3efe6aaddc887570
Ryan <55410510+RyanWestwood@users.noreply.github.com>
2023-02-22 22:48:43 +0000

    Changed boss to follow simple path

--- source/Boss.cpp
+++ source/Boss.cpp
@@ -47,9 +47,7 @@ void Boss::Input()
 
 void Boss::Update(const float delta_time)
 {
-  //Enemy::FollowPath(delta_time);
-  Enemy::FollowSmoothedPath(delta_time);
-
+  Enemy::FollowPath(delta_time);
 
   Enemy::Update(delta_time);
   // m_Tree.Update();
--- test/source/test_vector2.cpp
+++ test/source/test_vector2.cpp
@@ -3,21 +3,15 @@
 int main()
 {
   Vector2 vec{10.f, 10.f};
-  auto    mag  = vec.Magnitude();
-  auto    mag2 = Vector2::Magnitude({10, 10});
 
-  std::cout << vec;
-  std::cout << mag << "\n";
-  std::cout << mag2 << "\n";
+  float magnitude = Vector2::Magnitude({10, 10});
+  //if(!AreSame(magnitude, 14.1421f)) return 1;
 
-  auto a = Vector2::MoveTowards({10, 10}, {0, 0}, 1);
-  std::cout << a << "\n";
+  auto movement = Vector2::MoveTowards({10, 10}, {0, 0}, 1);
+  //if(movement != Vector2{9.29289f, 9.29289f}) return 1;
 
-  auto angle = Vector2::GetAngleBetween({768, 384}, {768, 712});
-  std::cout << angle << "\n";
-
-  auto angle2 = Vector2::GetAngleBetween({0, 712 - 384}, {0, 0});
-  std::cout << angle2 << "\n";
+  auto angle = Vector2::GetAngleBetween({0, 10}, {0, 0});
+  if(angle != 180) return 1;
 
   return 0;
 }
